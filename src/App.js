b841ebb0686b0ec4216470976559e5cb35df2222
Alex-Willenbrink <awillenbrink@gmail.com>
2017-08-22 11:41:56 -0500

    trying to make rows property in images display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 let images = require("./photos").data;
@@ -16,37 +15,83 @@ images = images.map(image => {
   };
 });
 
-const ImagePanel = ({
-  username,
-  imageUrl,
-  time,
-  likes,
-  comments,
-  filter,
-  tags
-}) => {
+let imageRow = [];
+let imageRows = [];
+imageRows = images.forEach((image, index) => {
+    imageRow.push(image)
+    if(index === images.length - 1 || imageRow.length > 2) {
+      imageRows.push(imageRow);
+      imageRow = [];
+    }
+})
+
+const ImagePanel = ({ image }) => {
   return (
-    <div className="panel panel-default">
+    <div className="panel panel-default col-xs-4">
       <div className="panel-body">
-        <img src={imageUrl} className="img-responsive" />
+        <img src={image.imageUrl} className="img-responsive" />
         <ul>
           <li>
-            {username}
+            {image.username}
           </li>
           <li>
-            {time}
+            {image.time}
           </li>
           <li>
-            Likes: {likes}
+            Likes: {image.likes}
           </li>
-          <li />
-          <li />
-          <li />
+          <li>
+            Comments: {image.comments}
+          </li>
+          <li>
+            Filter: {image.filter}
+          </li>
+          <li>
+            Tags: <ul className="list-inline"> { image.tags.map(tag => <li>{tag}</li>) } </ul>
+          </li>
         </ul>
       </div>
     </div>
   );
 };
+
+const ImageRow = ({ imageRow }) => {
+  return (
+    <div className="row">
+      {imageRow.map(image => <ImagePanel image={image}/>)}
+    </div>
+  )
+}
+
+const ImageContainer = ({ imageRows }) => {
+  return (
+    <div className="image">
+
+      {imageRows.map(imageRow => <ImageRow imageRow={imageRow}/>)}
+   </div>
+  )
+}
+
+// class imageContainer extends Component {
+//   constructor() {
+//     super()
+//
+//     this.state = {
+//       imageRow: image.slice(index, index + ((this.images.length - this.state.index) > 3) ? 3 : this.images.length)),
+//       index: 0,
+//       images: images
+//     }
+//   }
+//
+//
+//
+//   render() {
+//
+//   }
+
+
+// }
+
 
 class App extends Component {
   render() {
@@ -54,15 +99,21 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Our Photo Gallery!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <ImageContainer imageRows={imageRows} />
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+
+// proxyConsole.js:56 Warning: Unknown prop `imageRows` on <imageContainer> tag. Remove this prop from the element. For details, see https://fb.me/react-unknown-prop
+//     in imageContainer (at App.js:105)
+//     in div (at App.js:104)
+//     in div (at App.js:100)
+//     in App (at index.js:8)
